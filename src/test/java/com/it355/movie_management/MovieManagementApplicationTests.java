d2ebfede799515a089e0d5f8d4e1cfb47e6737dc
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2025-09-10 22:02:53 +0200

    Add files via upload

---
+++ src/test/java/com/it355/movie_management/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it355.movie_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
